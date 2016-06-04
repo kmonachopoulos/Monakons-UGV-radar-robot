d668bb02917af07bfe8807a902f48367b2b1d90f
Κωνσταντινος Μοναχοπουλος <kmonahopoulos@hotmail.gr>
2016-06-04 18:32:00 +0300

    Initial Commit

---
+++ MatlabRobotCommunication.m
@@ -0,0 +1,35 @@
+%%
+%--------------------------------------------------------------------------------------------------------------------------------
+% Technological Educational Institute of Athens - Xbee mini-project 
+%--------------------------------------------------------------------------------------------------------------------------------
+%%
+%%
+%--------------------------------------------------------------------------------------------------------------------------------
+% Project       : Monakons-UGV-Autonomus_Robot
+% File          : MatlabRobotCommunication.m
+% Description   : This code implements sets the communication parameters of arduino and matlab through a Serial port
+% Author        : Monahopoulos Konstantinos
+%--------------------------------------------------------------------------------------------------------------------------------
+%%
+
+clear;clc;
+S=serial('com18'); % Create an S Object
+data=0;
+set(S,'inputbuffersize',4096,'timeout',20); % Set serial communication parameter
+fopen(S); % Open serial communcation
+i=20:4:130;
+i=i.*(pi/180); % Convert deegres to rad (to plot using polar function)
+    
+while (1)
+    if s.bytesavailable>0 % If data from UGV are available
+    data=fscanf(S);
+    data = str2num(data);
+    figure,polar(i,data) % Plot the space infront of the UGV
+    end
+    data=0;
+end
+    
+% instrfind  % Checks if serial is activated
+% fclose(S); % Close the serial port
+% delete(S); % Delete S object
+% clear S;
---
+++ Monakons-UGV-radar-robot.ino
@@ -0,0 +1,162 @@
+/*
+-------------------------------------------------------------------------------------------------------------------------------
+Technological Educational Institute of Athens - Xbee mini-project  
+-------------------------------------------------------------------------------------------------------------------------------
+*/
+/*
+-------------------------------------------------------------------------------------------------------------------------------
+Project       : Monakons UGV_Autonomus_Robot
+File          : Monakons-UGV-radar-robot.ino
+Description   : This code implements UGV basic Code
+Author        : Monahopoulos Konstantinos
+-------------------------------------------------------------------------------------------------------------------------------
+*/
+
+#define sensorPin 0
+#include <Servo.h>
+#include <LiquidCrystal.h>
+
+// Initialize LCD Pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+Servo myservo;  // Create an instance of Class Servo
+
+// Initialize motor pins to Arduino
+int motor_pin1 = 8;
+int motor_pin2 = 9;
+int motor_pin3 = 6;
+int motor_pin4 = 7;
+int SensorValue;
+int distanceleft;
+int distance;
+int distanceright;
+String DataACquisition;
+String SpaceIt=" ";
+int i;
+
+void setup()
+{
+//  Initialize lcd screen
+lcd.begin(16, 2);
+Serial.begin (9600);
+pinMode(sensorPin, INPUT);
+    
+// Initialize Servo Pin and Position
+myservo.attach(10);
+myservo.write(74);
+    
+// Initialize motor Pins
+pinMode(motor_pin1,OUTPUT);
+pinMode(motor_pin2,OUTPUT);
+pinMode(motor_pin3,OUTPUT);
+pinMode(motor_pin4,OUTPUT);
+lcd.setCursor(0, 0);
+lcd.clear();
+lcd.print("arduino ZIGBEE");
+lcd.setCursor(0, 1);
+lcd.print("wireless robot");
+delay(10000);
+}
+
+void loop()
+{
+DataACquisition="";
+lcd.clear();
+lcd.setCursor(0, 0);
+lcd.print("scanning for obj");
+lcd.setCursor(0, 1);
+lcd.print("ect ");
+digitalWrite(motor_pin1,HIGH);
+digitalWrite(motor_pin2,LOW);
+SensorValue = analogRead(sensorPin);
+distance= 9462/(SensorValue - 16.92);
+
+// Move straight until you find an object
+if(distance<25 && distance>0)
+{
+    digitalWrite(motor_pin1,LOW);
+    digitalWrite(motor_pin2,HIGH);
+    delay(1000); // Breaks, this depends on the space that the robot moves.
+    digitalWrite(motor_pin1,LOW);
+    digitalWrite(motor_pin2,LOW);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("avoiding it...");
+    lcd.setCursor(0, 1);
+lcd.print("");
+delay(1000);
+
+// Scan from -90 to 90 degrees and detect the distances
+for (i=20;i<=130;i=i+4)
+    {
+
+    myservo.write(i);
+    delay(100);
+    SensorValue = analogRead(sensorPin);
+    if (i == 20)
+    {
+        distanceright= 9462/(SensorValue - 16.92);
+        }
+    if  (i == 128)
+        {
+            distanceleft= 9462/(SensorValue - 16.92);
+            }
+        distance= 9462/(SensorValue - 16.92);
+        DataACquisition = DataACquisition+SpaceIt;
+        DataACquisition=DataACquisition+distance;
+        }
+        // Send the data to Serial port while MatlabRobotCommunication.m is Running
+        Serial.println(DataACquisition);
+        myservo.write(74);
+
+        // Make a decision of the new path that the robot will move
+        if (distanceright<distanceleft)
+            {
+                lcd.clear();
+                lcd.setCursor(0, 0);
+                lcd.print("the object detec");
+                lcd.setCursor(0, 1);
+                lcd.print("ted at the right");
+                // Reverse
+                digitalWrite(motor_pin3,LOW);
+                digitalWrite(motor_pin4,HIGH);
+                digitalWrite(motor_pin1,LOW);
+                digitalWrite(motor_pin2,HIGH);
+                delay(1200);
+                // Stop reverse
+                digitalWrite(motor_pin1,HIGH);
+                digitalWrite(motor_pin2,LOW);
+                delay(500);
+                digitalWrite(motor_pin1,LOW);
+                digitalWrite(motor_pin2,LOW);
+                digitalWrite(motor_pin3,LOW);
+                digitalWrite(motor_pin4,LOW);
+                delay(1000);
+                }
+        else
+            {
+                lcd.clear();
+                lcd.setCursor(0, 0);
+                lcd.print("the object detec");
+                lcd.setCursor(0, 1);
+                lcd.print("ted at the left");
+                // Reverse
+                digitalWrite(motor_pin3,HIGH);
+                digitalWrite(motor_pin4,LOW);
+                digitalWrite(motor_pin1,LOW);
+                digitalWrite(motor_pin2,HIGH);
+                delay(1200);
+                // Stop reverse
+                digitalWrite(motor_pin1,HIGH);
+                digitalWrite(motor_pin2,LOW);
+                delay(500);
+                digitalWrite(motor_pin1,LOW);
+                digitalWrite(motor_pin2,LOW);
+                digitalWrite(motor_pin3,LOW);
+                digitalWrite(motor_pin4,LOW);
+                delay(1000);
+                }
+}
+}
+
+
